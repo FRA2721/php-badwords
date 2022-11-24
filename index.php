847e3386594f2457e8b576d2e7b47022e785d611
FRA2721 <cimino.francesco36@gmail.com>
2022-11-24 16:04:21 +0100

    test

---
+++ index.php
@@ -0,0 +1,32 @@
+<!-- php block code -->
+<?php 
+    echo "test";
+?>
+<!-- /php block code -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- /css -->
+
+    <!-- title -->
+    <title>PHP Badwords Request</title>
+    <!-- /title -->
+
+</head>
+
+<body>
+    <form action="response.php">
+        <button type="check">CHECK</button>
+    </form>
+</body>
+
+</html>
---
+++ response.php
@@ -0,0 +1,33 @@
+<!-- php block code -->
+<?php
+
+?>
+<!-- /php block code -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- /css -->
+
+    <!-- title -->
+    <title>PHP Badwords Response</title>
+    <!-- /title -->
+
+</head>
+
+<body>
+
+    <a href="index.php">
+        <h2>WELL DONE!</h2>
+    </a> 
+
+</body>
+</html>
